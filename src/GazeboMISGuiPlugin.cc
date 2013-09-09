2b00dc9809a8e2a220661ec8cad569e7a9598aed
ahb <andreas.bihlmaier@gmx.de>
2013-09-09 10:05:01 +0200

    bring plugin and lwr_* together

--- src/GazeboMISGuiPlugin.cc
+++ src/GazeboMISGuiPlugin.cc
@@ -22,10 +22,11 @@ namespace gazebo
       virtual void Init()
       {
         std::cout << "------------------- GazeboMISGuiPlugin -------------------" << std::endl;
-        std::cout << "Adding OGRE head to scene" << std::endl;
         rendering::ScenePtr scene = rendering::RenderEngine::Instance()->GetScene();
         Ogre::SceneManager* sceneManager = scene->GetManager();
 
+#if 0
+        std::cout << "Adding OGRE head to scene" << std::endl;
         Ogre::Root *root = rendering::RenderEngine::Instance()->root;
         Ogre::ResourceGroupManager::getSingleton().addResourceLocation("/usr/share/OGRE-1.7.4/media/models/", "FileSystem");
         Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
@@ -33,6 +34,7 @@ namespace gazebo
         Ogre::Entity* headEntity = sceneManager->createEntity("Head", "ogrehead.mesh");
         Ogre::SceneNode* headNode = sceneManager->getRootSceneNode()->createChildSceneNode("HeadNode");
         headNode->attachObject(headEntity);
+#endif // 0
       }
   };
 
